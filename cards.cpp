c9e164136d71a7a5a36a85211af7c2a4d9413c22
Lucas Nguyen <lucasnguyen@ucsb.edu>
2022-05-02 02:25:03 -0700

    added starter code files

---
+++ cards.cpp
@@ -0,0 +1,4 @@
+//cards.cpp
+//Authors: Your name and your partner's name
+//Implementation of the classes defined in cards.h
+
---
+++ cards.h
@@ -0,0 +1,8 @@
+//cards.h
+//Authors: Your name and your partner's name
+//All class declarations go here
+
+#ifndef CARDS_H
+#define CARDS_H
+
+#endif
