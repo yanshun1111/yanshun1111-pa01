7e1f188a392f1b078a6bcae3e6a15e869aedc13b
lucastnguyen <43656395+lucastnguyen@users.noreply.github.com>
2022-05-05 02:41:45 -0700

    Update cards.h

--- cards.h
+++ cards.h
@@ -1,6 +1,6 @@
-//cards.h
-//Authors: Your name and your partner's name
-//All class declarations go here
+// cards.h
+// Author: Your name
+// All class declarations go here
 
 #ifndef CARDS_H
 #define CARDS_H
